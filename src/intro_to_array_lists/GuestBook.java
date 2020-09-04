44d39ef6772ddb5ca66f8b5de5f0260fbbd1dda9
Gizemtokgoz <48003456+Gizemtokgoz@users.noreply.github.com>
2020-09-03 18:18:30 -0700

    GuestBook, IntroToArrayLists :smile:

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,12 +1,65 @@
 package intro_to_array_lists;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
-	// all the names added to the list. Format the list as follows:
-	// Guest #1: Bob Banders
-	// Guest #2: Sandy Summers
-	// Guest #3: Greg Ganders
-	// Guest #4: Donny Doners
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
+	Frame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
+	ArrayList<String> names = new ArrayList<String>();
+
+	public void run() {
+		// Create a GUI with two buttons. One button reads "Add Name" and the other
+		// button reads "View Names".
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(button1);
+		panel.add(button2);
+		button1.setText("add name");
+		button2.setText("view names");
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		button1.setPreferredSize(new Dimension(200, 50));
+		button2.setPreferredSize(new Dimension(200, 50));
+		frame.pack();
+		// When the add name button is clicked, display an input dialog that asks the
+		// user to enter a name. Add
+		// that name to an ArrayList. When the "View Names" button is clicked, display a
+		// message dialog that displays
+		// all the names added to the list. Format the list as follows:
+		// Guest #1: Bob Banders
+		// Guest #2: Sandy Summers
+		// Guest #3: Greg Ganders
+		// Guest #4: Donny Doners
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) arg0.getSource();
+		String name;
+
+		if (button1 == buttonPressed) {
+			name = JOptionPane.showInputDialog("enter a name");
+			names.add(name);
+		}
+		
+		if (button2 == buttonPressed) {
+			String guests = "";
+			for (int j = 1; j < names.size(); j++) {
+				guests += "guest # " + j + " " + names.get(j) + "\n";
+			}
+			JOptionPane.showMessageDialog(null, guests);
+		}
+	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,51 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
-		
-		//2. Add five Strings to your list
-		
-		//3. Print all the Strings using a standard for-loop
-		
-		//4. Print all the Strings using a for-each loop
-		
-		//5. Print only the even numbered elements in the list.
-		
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
+		ArrayList<String> list = new ArrayList<String>();
+		String thing1 = "zero";
+		String thing2 = "one";
+		String thing3 = "two";
+		String thing4 = "three";
+		String thing5 = "four";
+
+		list.add(thing1);
+		list.add(thing2);
+		list.add(thing3);
+		list.add(thing4);
+		list.add(thing5);
+		// 2. Add five Strings to your list
+		// list(1) = "";
+		// 3. Print all the Strings using a standard for-loop
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+		// 4. Print all the Strings using a for-each loop
+		for (String string : list) {
+			System.out.println(string);
+		}
+		// 5. Print only the even numbered elements in the list.
+		System.out.println("........");
+		for (int i = 0; i < list.size(); i++) {
+			if (i % 2 == 0) {
+				System.out.println(list.get(i));
+			}
+		}
+		System.out.println("........");
+		// 6. Print all the Strings in reverse order.
+		for (int i = 4; i >= 0; i--) {
+			System.out.println(list.get(i));
+		}
+		System.out.println("........");
+		// 7. Print only the Strings that have the letter 'e' in them.
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).contains("e")) {
+				System.out.println(list.get(i));
+			}
+		}
 	}
 }
